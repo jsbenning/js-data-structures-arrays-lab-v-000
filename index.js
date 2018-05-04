d7407c1a0a955037c952c26e43a037e179cb4f86
Josef Benninghoff <jsbenning4@gmail.com>
2018-05-04 16:18:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ function destructivelyRemoveFirstDriver(){
 }
 
 function appendDriver(name){
-  let newDrivers = [name, ...drivers];
+  let newDrivers = [...drivers, name];
   return newDrivers;
 }
